8362bf99503c24e46cdb49826cb7e32355af6153
Yashmyn Jackson <yashmyn@gmail.com>
2015-07-11 17:04:32 -0400

    Short and simple introductory WriteLine program.

---
+++ 2015-07-06-HelloWorldLab/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2015_07_06_HelloWorldLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ask user to input first and last name
+
+            Console.WriteLine();
+            Console.Write("What is your first name? ");
+            String firstName = Console.ReadLine();
+            Console.Write("What is your last name? ");
+            String lastName = Console.ReadLine();
+            Console.WriteLine();
+
+            //write hello and remain on same line
+
+            Console.Write("Hello ");
+
+            //write first and last name entered
+
+            Console.WriteLine(firstName + " " + lastName + "!");
+
+            //keep the console open
+
+            Console.ReadLine();
+            
+
+        }
+    }
+}
